ac8db24a030a61ba3ee769cfcb710ee8f1b45a5e
jlaluezas <jlalueza@cyberclick.net>
2021-05-26 18:01:31 +0200

    css ok

--- app/src/Command/StepInfoCommand.php
+++ app/src/Command/StepInfoCommand.php
@@ -4,7 +4,27 @@
 namespace App\Command;
 
 
-class StepInfoCommand
+class StepInfoCommand extends Command
 {
+    protected static $defaultName = 'app:step:info';
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at',
+                'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+        });
+        $output->writeln($step);
+        return 0;
+    }
 
 }
