6c7e06d0921a3686158bef2d167c30ff4a7eef48
carlotapg <cpinol@cyberclick.net>
2021-06-02 15:20:37 +0200

    step 29

--- app/src/Controller/ConferenceController.php
+++ app/src/Controller/ConferenceController.php
@@ -9,8 +9,8 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -22,7 +22,6 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-
     private $twig;
     private $entityManager;
     private $bus;
@@ -34,37 +33,42 @@ class ConferenceController extends AbstractController
         $this->bus = $bus;
     }
 
-    /** @Route("/", name="homepage") */
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'en']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         $response = new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
         $response->setSharedMaxAge(3600);
+
         return $response;
     }
 
-    /** @Route("/conference_header", name="conference_header") */
-    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    #[Route('/{_locale<%app.supported_locales%>}/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
         $response = new Response($this->twig->render('conference/header.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
         $response->setSharedMaxAge(3600);
+
         return $response;
     }
 
-    /** @Route("/conference/{slug}", name="conference") */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository,
-                         NotifierInterface $notifier, string $photoDir)
+    #[Route('/{_locale<%app.supported_locales%>}/conference/{slug}', name: 'conference')]
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, NotifierInterface $notifier, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
-
             if ($photo = $form['photo']->getData()) {
                 $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
                 try {
@@ -72,9 +76,9 @@ class ConferenceController extends AbstractController
                 } catch (FileException $e) {
                     // unable to upload the photo, give up
                 }
-
                 $comment->setPhotoFilename($filename);
             }
+
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
 
@@ -85,30 +89,27 @@ class ConferenceController extends AbstractController
                 'permalink' => $request->getUri(),
             ];
 
-            $reviewUrl = $this->generateUrl('review_comment', ['id' =>
-                $comment->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
-            $this->bus->dispatch(new CommentMessage($comment->getId(),
-                $reviewUrl, $context));
-            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after 
-            moderation.', ['browser']));
+            $reviewUrl = $this->generateUrl('review_comment', ['id' => $comment->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $reviewUrl, $context));
+
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
+
         if ($form->isSubmitted()) {
-            $notifier->send(new Notification('Can you check your submission? There are some problems with it.',
-                ['browser']));
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-        return $this->render('conference/show.html.twig',
-            ['conference' => $conference,
-                'comments' => $paginator,
-                'previous' => $offset,
-                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
-                'comment_form' => $form->createView(),
-            ]);
-
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
+        ]));
     }
 }
